4ccf5f37afc76851181ab9f332b5191b09bb9cf0
Sean Bhupathi <seanbhupathi@gmail.com>
2017-06-12 15:41:18 -0400

    added projectile collider, destroys missile on hit of enemy and enemy dies after 2 hits thanks to its health compared to missile damage. Only if collide with missile nothing else

---
+++ Assets/Entities/Enemy/EnemyBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyBehaviour : MonoBehaviour {
+
+	public float health = 150f;
+
+	void OnTriggerEnter2D(Collider2D collider){
+		Projectile missile = collider.gameObject.GetComponent<Projectile>();
+		if(missile){
+			health -= missile.GetDamage();
+			missile.Hit();
+			if (health <= 0){
+				Destroy(gameObject);
+			}
+		}
+	}
+}
